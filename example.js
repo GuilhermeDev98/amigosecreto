e5b979b50e983afdeba374e28eb3046c3fdf6c8e
Guilherme Santos <guilhermedev@hotmail.com>
2019-12-05 22:23:44 -0300

    first commit

---
+++ example.js
@@ -0,0 +1,18 @@
+const AmigoSecreto = require('./AmigoSecreto')
+
+const participantes = [
+    'Ariane',
+    'Lyndi',
+    'Jamile',
+    'Erisson',
+    'Wedicarlos',
+    'Jairlaine',
+    'Rodrigo',
+    'Blenda',
+    'Raniele',
+    'Julio',
+    'Tawane',
+    'Jean',
+]
+
+AmigoSecreto(participantes)
